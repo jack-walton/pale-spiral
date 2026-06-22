2a75475b1c182774b6c9423c7cbda0003d3c01b0
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2026-06-22 15:12:29 -0400

    Change domain to jackwalton.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jackwalton.xyz',
+	site: 'https://jackwalton.net',
 	integrations: [
 		starlight({
 			components: {
